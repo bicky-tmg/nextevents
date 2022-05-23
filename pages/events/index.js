0f97a741e63f64d281f44502692660f59408f96c
Bicky Tamang <bickytamang101@gmail.com>
2022-05-23 20:37:47 +0545

    Adding a Filter Form

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,15 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 
 function AllEventsPage() {
   const events = getAllEvents();
 
   return (
-    <div>
+    <>
+      <EventsSearch />
       <EventList items={events} />
-    </div>
+    </>
   );
 }
 
