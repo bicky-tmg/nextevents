01c54dc42b9af0454835a322a932d2c6d48127b7
Bicky Tamang <bickytamang101@gmail.com>
2022-05-26 16:15:08 +0545

    Optimizing Data Fetching

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,16 +1,15 @@
-import { getEventById, getAllEvents } from "../../helpers/api-util";
+import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
-import ErrorAlert from "../../components/ui/error-alert";
 
 function EventDetailPage({ event }) {
   if (!event) {
     return (
       <>
-        <ErrorAlert>
-          <p>No event found!</p>
-        </ErrorAlert>
+        <div className="center">
+          <p>Loading...</p>
+        </div>
       </>
     );
   }
@@ -40,17 +39,18 @@ export async function getStaticProps(context) {
     props: {
       event,
     },
+    revalidate: 30,
   };
 }
 
 export async function getStaticPaths() {
-  const events = await getAllEvents();
+  const events = await getFeaturedEvents();
 
   const paths = events.map((event) => ({ params: { eventId: event.id } }));
 
   return {
     paths,
-    fallback: false,
+    fallback: "blocking",
   };
 }
 
--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export async function getStaticProps() {
     props: {
       featuredEvents,
     },
+    revalidate: 1800,
   };
 }
 
