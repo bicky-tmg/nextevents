92cf63474eed175c3d7e6e0285ed29f35341edca
Bicky Tamang <bickytamang101@gmail.com>
2022-05-27 11:48:00 +0545

    Adding Dynamic head content

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
+import Head from "next/head";
+
 import EventList from "../components/events/event-list";
 import { getFeaturedEvents } from "../helpers/api-util";
 
 function HomePage({ featuredEvents }) {
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve..."
+        />
+      </Head>
       <EventList items={featuredEvents} />
     </div>
   );
