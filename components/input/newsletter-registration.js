69d9d9355a652cea76f8f6b1ce457b6d215c01ec
Bicky Tamang <bickytamang101@gmail.com>
2022-05-31 16:08:39 +0545

    Triggering and showing notificatons

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,13 +1,21 @@
-import { useRef } from "react";
+import { useRef, useContext } from "react";
 import classes from "./newsletter-registration.module.css";
+import NotificationContext from "../../store/notification-context";
 
 function NewsletterRegistration() {
   const emailInputRef = useRef();
+  const notificationCtx = useContext(NotificationContext);
 
   function registrationHandler(event) {
     event.preventDefault();
 
     const enteredEmail = emailInputRef.current.value;
+
+    notificationCtx.showNotification({
+      title: "Signing up...",
+      message: "Registering for newsletter.",
+      status: "pending",
+    });
 
     fetch("/api/newsletter", {
       method: "POST",
@@ -16,8 +24,29 @@ function NewsletterRegistration() {
         "Content-Type": "application/json",
       },
     })
-      .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+
+        return res.json().then((data) => {
+          throw new Error(data.message || "Something went wrong!");
+        });
+      })
+      .then((data) => {
+        notificationCtx.showNotification({
+          title: "Success!",
+          message: "Successfully registered for newsletter.",
+          status: "success",
+        });
+      })
+      .catch((e) => {
+        notificationCtx.showNotification({
+          title: "Error",
+          message: e.message || "Something went wrong!",
+          status: "error",
+        });
+      });
   }
 
   return (
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Layout from "../components/layout/layout";
-import { NotificationContextProvider } from "../components/ui/notification";
+import { NotificationContextProvider } from "../store/notification-context";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
